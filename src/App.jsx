e929a9a00603b0dbad8741b8a471472127d12731
nishant2147 <bishtuma147@gmai.com>
2026-03-12 17:37:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SIgnUp from './Auth/SIgnUp';
+
+function App() {
+  return (
+    <div className="App">
+      <SIgnUp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Auth/SIgnUp.jsx
@@ -0,0 +1,15 @@
+import { Formik } from 'formik'
+import React from 'react'
+
+const SIgnUp = () => {
+
+  return (
+    <div>
+        <h1>Sign Up</h1>    
+           <Formik>
+            </Formik> 
+    </div>
+  )
+}
+
+export default SIgnUp
